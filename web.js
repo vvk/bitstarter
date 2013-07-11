79702e2519ebc57af7b8080fe444dab7d8aa4af7
Vladimir Khanevich <vladimir.khanevich@gmail.com>
2013-07-11 17:14:45 +0000

    hello from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var hello = fs.readFileSync('index.html');
+  
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
