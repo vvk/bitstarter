470d3137f05f78dcd78d65ecc530e9745af1b814
Vladimir Khanevich <vladimir.khanevich@gmail.com>
2013-07-11 18:00:12 +0000

    test

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var buf = new Buffer('test');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var hello = fs.readFileSync('index.html');
   
-  response.send(hello);
+  response.send(hello.toString());
 });
 
 var port = process.env.PORT || 5000;
